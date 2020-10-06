62954dc7145b9f1f609cd99f2d6dfbef21c0c1eb
shradha-Bacchewar <68393987+shradha-Bacchewar@users.noreply.github.com>
2020-10-06 20:25:34 +0530

    Add files via upload

---
+++ Assignment_8/Q53.java
@@ -0,0 +1,28 @@
+package p52;
+
+import java.util.Scanner;
+
+public class Q53 {
+
+	        public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the string input : ");
+		String input = sc.nextLine();
+
+		String arr[] = input.split(" ");
+
+		String res = "";
+
+		for(String trav : arr) 
+                {			
+                        String first = trav.substring(0,1);
+			String rest = trav.substring(1);
+			res = res+first.toUpperCase()+rest.toLowerCase()+" ";
+	        }
+
+		System.out.println(res);
+}
+
+}
